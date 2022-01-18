7c97688061c3d34abd8b17009fac9f048003d5a1
humorousdragon <deepakrawat.hmr@gmail.com>
2022-01-18 21:51:51 +0530

    eks,output,requirements-app,sg,version

---
+++ sg.tf
@@ -0,0 +1,46 @@
+resource "aws_sg" "worker_one" {
+  name_prefix = "worker_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_sg" "worker_two" {
+  name_prefix = "worker_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_sg" "all_worker" {
+  name_prefix = "all_worker"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
