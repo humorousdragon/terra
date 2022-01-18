019ed36b5ccd9e724ce10ee610ec3fed4857fedc
humorousdragon <deepakrawat.hmr@gmail.com>
2022-01-18 22:38:03 +0530

    azs

--- vpc.tf
+++ vpc.tf
@@ -30,7 +30,8 @@ module "vpc" {
   name = "dragon-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = data.aws_availibility_zones.available.names
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+#  data.aws_availibility_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
